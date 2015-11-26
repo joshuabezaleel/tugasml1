8ef6a2e74165a66ac4b230bc4d1336afd63bad20
lindasekawati <sekawati.linda@gmail.com>
2015-11-26 21:44:41 +0700

    Add ArrayList

--- src/classifier/neuralnetwork/Node.java
+++ src/classifier/neuralnetwork/Node.java
@@ -5,14 +5,17 @@
  */
 package classifier.neuralnetwork;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author LINDA
  */
 public class Node {
     private float nodeVal;          //nilai pada node
-    // List private int nodeIn;     //list node yang menjadi input
-    // List private int nodeOut;    //list node yang menjadi output
+    private List nodeIn = new ArrayList();     //list node yang menjadi input
+    private List nodeOut = new ArrayList();    //list node yang menjadi output
     private  float weight;          //nilai weight node terhadap node output
     
     public Node(){
@@ -23,6 +26,14 @@ public class Node {
     public float getNodeVal(){      //mengembalikan nilai nodeVal
         return nodeVal;         
     }
-    
+    public Object getNodeIn(int i){
+        Object object = nodeIn.get(i);
+        return object;
+    }
+    public float getWeight(int i){
+        Object object = nodeIn.get(i);
+        Node node = (Node) object;
+        return node.weight;
+    }
     
 }
