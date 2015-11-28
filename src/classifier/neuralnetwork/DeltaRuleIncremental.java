573c493f92be3bfd531160c89761886284886bb3
Joshua Bezaleel Abednego <joshua.bezaleel@gmail.com>
2015-11-28 12:42:53 +0700

    add class for each classifier

---
+++ src/classifier/neuralnetwork/DeltaRuleIncremental.java
@@ -0,0 +1,15 @@
+package classifier.neuralnetwork;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class DeltaRuleIncremental {
+    
+}
